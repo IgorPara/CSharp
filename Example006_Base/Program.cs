b1ace520e9dfc38b2d151a7ed46e7928346a3a70
FL <l.u.1980@mail.ru>
2023-11-30 02:31:30 +0300

    Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 2;
+int b = 72;
+int c = 2;
+int d = 545;
+int e = 2;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
